06e01c57764f681233ba97bfc4be3ae5a6374fb6
Harji <harji.virk@gmail.com>
2023-03-21 10:05:08 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//import Users from './user/pages/Users';
+//import NewPlace from './places/pages/NewPlace';
+//import UserPlaces from './places/pages/UserPlaces';
+//import UpdatePlace from './places/pages/UpdatePlace';
+//import Auth from './user/pages/Auth';
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import LoadingSpinner from "./shared/components/UIElements/LoadingSpinner";
+import { AuthContext } from "./shared/context/auth-context";
+import { useAuth } from "./shared/hooks/auth-hook";
+
+const Users = React.lazy(() => import("./user/pages/Users"));
+const NewPlace = React.lazy(() => import("./places/pages/NewPlace"));
+const UserPlaces = React.lazy(() => import("./places/pages/UserPlaces"));
+const UpdatePlace = React.lazy(() => import("./places/pages/UpdatePlace"));
+const Auth = React.lazy(() => import("./user/pages/Auth"));
+
+const App = () => {
+  const { token, login, logout, userId } = useAuth();
+  let routes;
+  if (token) {
+    routes = (
+      <Routes>
+        <Route exact path="/" element={<Users />} />
+        <Route exact path="/:userId/places" element={<UserPlaces />} />
+        <Route exact path="/places/new" element={<NewPlace />} />
+        <Route exact path="/places/:placeId" element={<UpdatePlace />} />
+        <Route path="*" element={<Users />} />
+      </Routes>
+    );
+  } else {
+    routes = (
+      <Routes>
+        <Route exact path="/" element={<Users />} />
+        <Route exact path="/:userId/places" element={<UserPlaces />} />
+        <Route exact path="/auth" element={<Auth />} />
+        <Route path="*" element={<Auth />} />
+      </Routes>
+    );
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: !!token,
+        token: token,
+        userId: userId,
+        login: login,
+        logout: logout,
+      }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>
+          <Suspense
+            fallback={
+              <div className="center">
+                <LoadingSpinner />
+              </div>
+            }
+          >
+            {routes}
+          </Suspense>
+        </main>
+      </Router>
+    </AuthContext.Provider>
+  );
+};
+
+export default App;
